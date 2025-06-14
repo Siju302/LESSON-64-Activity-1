013aa94c2afe70d145008bcd3ff837fc844bb1db
Siju302 <sijuadeoyelami10@gmail.com>
2025-06-14 12:50:18 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+light = input("Enter 1 to turn ON the light, 0 to turn it OFF: ")
+
+if light == "1":
+    print("💡 Light is ON")
+elif light == "0":
+    print("🌑 Light is OFF")
+else:
+    print("Only 0 or 1 is allowed!")
